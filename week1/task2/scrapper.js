6491a2bd02d280e6c837bcf454044a9fa2bd6860
Plamen Todorov <pl.todorov@gmail.com>
2014-10-27 16:16:16 +0200

    scrapper improvements

--- week1/task2/scrapper.js
+++ week1/task2/scrapper.js
@@ -1,99 +1,99 @@
+// My timer execution time is 0 seconds and 673.776067 milliseconds.
 var debug = require('debug')('scrapper');
-var _ = require('underscore');
-var read = require('../../shared/readers/reader-local');
-var write = require('../../shared/writers/writer-file');
-var http = require('http');
-var https = require('https');
-var collection = 'articles.json';
-var db = require('../../shared/db/facade-node-persist')([collection]);
+var timer = require('../../shared/timer')();
 var api = require('../../shared/api/facade-firebase');
 
-var maxItemFile = 'persist/maxitem.txt';
-var lastItemUsed = parseInt(read.sync(maxItemFile));
-var ids = [];
+run();
 
-runProcess();
-setInterval(function() {
-  runProcess();
-}, 60 * 2000);
-
-function runProcess() {
-  step1_getLatestMaxItem();
+function run() {
+  function allDone() {
+    console.log('Scrapping done. Will run after two minutes again.')
+    setTimeout(function() {
+      s1_identifyPosts(allDone);
+    }, 60 * 3000);
+  }
+  s1_identifyPosts(allDone);
 }
 
-function step1_getLatestMaxItem() {
-  api.getLatestPostId(function (err, id) {
-    var currentItem = parseInt(id);
-    debug('Current Last Item: ' + currentItem);
-    debug('Last Item Used: ' + lastItemUsed);    
-    //Limit the items to 100
-    if ((currentItem - lastItemUsed) > 100 ) {
-      ids = _.range(currentItem - 99, currentItem + 1);
-    } else {
-      ids = _.range(lastItemUsed + 1, currentItem + 1);
+function s1_identifyPosts(allDone) {
+  api.getLatestPostId(function(err, id) {
+    var ids = getPostIds(parseInt(id));
+    if (ids.length === 0) {
+      console.log('No new data at this moment');
+      return;
     }
-    write(maxItemFile, currentItem);
-    step2_getAllArticles();
+    s2_getPosts(
+      ids,
+      s3_savePosts,
+      allDone
+    );
   });
 }
 
-
-function step2_getAllArticles() {
+function s2_getPosts(ids, done, allDone) {
   var expectedRequests = ids.length;
-  debug('Expected Requests: ' + expectedRequests);
   var finishedRequests = 0;
-  if (expectedRequests === 0) {
-    console.log('No new data at this moment');
-  } else {
-    _.each(ids, function (id) {
-      api.getPost(id, function (err, post) {
-        var dataJSON;
-        finishedRequests = finishedRequests + 1;
-        debug('Finished Requests: ' + finishedRequests);
-        try {
-          debug(post);
-          dataJSON = JSON.parse(post)
-        } catch(e) {
-          //Some of the responses have incorrect JSON - skip these
-          return;
-        }
-        step3_ProcessFoundData(dataJSON, (expectedRequests === finishedRequests));
-      })
+  var posts = [];
+  debug('Expected Requests: ' + expectedRequests);
+
+  timer.start();
+  ids.forEach(function(id) {
+    api.getPost(id, function(err, post) {
+      var dataJSON = JSON.parse(post)
+      finishedRequests = finishedRequests + 1;
+      debug('Finished Requests: ' + finishedRequests);
+      (dataJSON.type === 'story' || dataJSON.type === 'comment') && posts.push(dataJSON);
+      (expectedRequests === finishedRequests) && s3_savePosts(posts, allDone);
     })
-  }
+  })
 }
 
-function step3_ProcessFoundData(data, isLast) {
-  if (data.type === 'story' || data.type === 'comment') {
-    db.insertOne(collection, data, false);
-  }
-  if (isLast) {
-    //wakeUpNotifier();
-    console.log('Wake up notifier');
-  }
+function s3_savePosts(posts, allDone) {
+  var collection = 'articles.json';
+  var db = require('../../shared/db/facade-node-persist')([collection]);
+  db.insertMany(collection, posts, false);
+  console.log(db.findAll(collection));
+  process.nextTick(s4_wakeUpNotifier);
+  allDone();
 }
 
-function wakeUpNotifier() {
-  var options = {
-    hostname: 'localhost',
-    port: 3001,
-    method: 'POST',
-    path: '/newArticles'    
-  };
-  
-  // Small delay to ensure that all is in articles.json
-  setTimeout(function () {
-    debug('Waking up notifier...');
-    var req = http.request(options, function(res) {
-      res.on('data', function (chunk) {
-        console.log(chunk.toString());
-      });
-    });
-  
-    req.on('error', function(e) {
-      console.log('Problem with request: ' + e.message);
-    });
-  
-    req.end();    
-  }, 10000)
+function s4_wakeUpNotifier() {
+  var invoke = require('../../shared/readers/reader-remote');
+  invoke.httpPost(
+    {
+      hostname: 'localhost',
+      port: 3001,
+      method: 'POST',
+      path: '/newArticles'
+    },
+    function (err, data) {
+      err ? console.log(err) : console.log(data)
+    }
+  );
+}
+
+function getLastUsedId(latestId) {
+  var read = require('../../shared/readers/reader-local');
+  var write = require('../../shared/writers/writer-file');
+  var maxItemFile = 'persist/maxitem.txt';
+  var lastUsedId = parseInt(read.sync(maxItemFile));
+
+  write(maxItemFile, latestId);
+  return lastUsedId;
+}
+
+function getPostIds(id) {
+  var _ = require('underscore');
+  var latestId = parseInt(id);
+  var lastUsedId = getLastUsedId(latestId);
+
+  debug('Current Last Item: ' + latestId);
+  debug('Last Item Used: ' + lastUsedId);
+
+  //Limit the items to last 100 if the process has not been started for too long
+  if ((latestId - lastUsedId) > 100) {
+    return _.range(latestId - 99, latestId + 1);
+  } else {
+    return _.range(lastUsedId + 1, latestId + 1);
+  }
 }
